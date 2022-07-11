19d2f9665efaa8f4d87914b4da44f0bc21007b56
regex <aditya777regex@gmail.com>
2022-07-11 22:56:04 +0530

    Fixed somethn

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.get('/', (req, res) => {
         const category = response.data.meals[0].strCategory;
         const instructions = response.data.meals[0].strInstructions;
         const ingredients = response.data.meals[0].strIngredient1 + ' ' + response.data.meals[0].strIngredient2 + ' ' + response.data.meals[0].strIngredient3 + ' ' + response.data.meals[0].strIngredient4 + ' ' + response.data.meals[0].strIngredient5 + ' ' + response.data.meals[0].strIngredient6 + ' ' + response.data.meals[0].strIngredient7 + ' ' + response.data.meals[0].strIngredient8 + ' ' + response.data.meals[0].strIngredient9 + ' ' + response.data.meals[0].strIngredient10 + ' ' + response.data.meals[0].strIngredient11 + ' ' + response.data.meals[0].strIngredient12 + ' ' + response.data.meals[0].strIngredient13 + ' ' + response.data.meals[0].strIngredient14 + ' ' + response.data.meals[0].strIngredient15 + ' ' + response.data.meals[0].strIngredient16 + ' ' + response.data.meals[0].strIngredient17 + ' ' + response.data.meals[0].strIngredient18 + ' ' + response.data.meals[0].strIngredient19 + ' ' + response.data.meals[0].strIngredient20;
-        res.render('index', {name, image, category, instructions, ingredients, cuisine});
+        const src = response.data.meals[0].strSource;
+        res.render('index', {name, image, category, instructions, ingredients, cuisine, src});
     })
 });
 
